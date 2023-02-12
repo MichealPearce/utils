e4b49633fac4d000992c1d3c7fc15776cd785d90
Micheal Pearce <michealpearce@outlook.com>
2023-02-12 18:04:57 -0500

    update EventBus

--- src/classes/EventBus.ts
+++ src/classes/EventBus.ts
@@ -1,4 +1,4 @@
-import { Rollable } from '../functions'
+import { roll, Rollable } from '../functions'
 import { FunctionType } from '../types'
 
 export type EventBusFunction = (...args: any[]) => Rollable
@@ -9,7 +9,7 @@ export type EventBusMap<Hooks extends Record<string, FunctionType>> = Map<
 >
 
 export class EventBus<Events extends Record<string, EventBusFunction>> {
-	constructor(public eventMap: EventBusMap<Events> = new Map()) {}
+	constructor(public readonly eventMap: EventBusMap<Events> = new Map()) {}
 
 	on<Event extends keyof Events>(
 		event: Event,
@@ -23,21 +23,37 @@ export class EventBus<Events extends Record<string, EventBusFunction>> {
 		const levelSet = eventMap.get(level)!
 
 		levelSet.add(handler)
-
-		return () => levelSet.delete(handler)
+		return () => this.off(event, handler, level)
 	}
 
-	trigger<Event extends keyof Events>(
-		event: Event,
-		...args: Parameters<Events[Event]>
-	) {
+	off(event: keyof Events, handler: Events[keyof Events], level?: number) {
 		if (!this.eventMap.has(event)) return
 		const eventMap = this.eventMap.get(event)!
 
-		for (const levelSet of eventMap.values()) {
-			for (const handler of levelSet) {
-				handler(...args)
-			}
+		if (level) {
+			if (!eventMap.has(level)) return
+			const levelSet = eventMap.get(level)!
+			levelSet.delete(handler)
+			return
 		}
+
+		for (const [_level, levelSet] of eventMap) {
+			if (levelSet.has(handler)) levelSet.delete(handler)
+		}
+	}
+
+	emit<Event extends keyof Events>(
+		event: Event,
+		...args: Parameters<Events[Event]>
+	): Rollable {
+		if (!this.eventMap.has(event)) return
+		const eventMap = this.eventMap.get(event)!
+
+		return roll(
+			Array.from(eventMap)
+				.sort(([levelA], [levelB]) => levelA - levelB)
+				.flatMap(([_, levelSet]) => Array.from(levelSet))
+				.map(handler => () => handler(...args)),
+		)
 	}
 }
