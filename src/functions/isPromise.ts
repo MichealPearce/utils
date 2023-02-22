17d8ccc36101a198c0c5938eb8061504ff26176c
Micheal Pearce <michealpearce@outlook.com>
2023-02-22 13:31:58 -0500

    implement isPromiseLike and notPromiseLike

--- src/functions/isPromise.ts
+++ src/functions/isPromise.ts
@@ -1,10 +1,20 @@
+import { inObject } from './inObject'
 import { Is, Not, not } from './is'
+import { isFunction } from './isFunction'
 import { isObject } from './isObject'
 
 export function isPromise<T>(thing: T): thing is Is<T, Promise<any>> {
 	return isObject(thing) && thing instanceof Promise
 }
 
+export function isPromiseLike<T>(thing: T): thing is Is<T, PromiseLike<any>> {
+	return isObject(thing) && inObject(thing, ['then']) && isFunction(thing.then)
+}
+
 export function notPromise<T>(thing: T): thing is Not<T, Promise<any>> {
 	return not(isPromise(thing))
+}
+
+export function notPromiseLike<T>(thing: T): thing is Not<T, PromiseLike<any>> {
+	return not(isPromiseLike(thing))
 }
