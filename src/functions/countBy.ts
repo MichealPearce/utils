5c93fa1f22f37bdf48517dc1ec6ad14a172b7e34
Micheal Pearce <michealpearce@outlook.com>
2023-02-27 15:10:56 -0500

    rework countBy to return Map instead of plain object

--- src/functions/countBy.ts
+++ src/functions/countBy.ts
@@ -1,42 +1,32 @@
 import { toEntries } from './entries'
 
 export type CountArrayByHandler<Collection extends Array<any>> = (
-	key: number,
 	value: Collection[number],
-) => Collection[number]
+	key: number,
+) => any
 
 export type CountObjectByHandler<Collection extends object> = (
-	key: keyof Collection,
 	value: Collection[keyof Collection],
-) => Collection[keyof Collection]
+	key: keyof Collection,
+) => any
 
 export type CountByHandler<Collection extends Array<any> | object> =
 	Collection extends Array<any>
 		? CountArrayByHandler<Collection>
 		: CountObjectByHandler<Collection>
 
-export type CountRecord<Collection extends Array<any> | object> = Record<
-	Collection extends Array<any>
-		? Collection[number]
-		: Collection[keyof Collection],
-	number
->
-
-export function countBy<Collection extends Array<any> | object>(
-	collection: Collection,
-	handler: Collection extends Array<any>
-		? CountArrayByHandler<Collection>
-		: CountObjectByHandler<Collection>,
-): CountRecord<Collection> {
-	const count: CountRecord<Collection> = {} as any
+export function countBy<
+	Collection extends Array<any> | object,
+	Handler extends CountByHandler<Collection>,
+>(collection: Collection, handler: Handler): Map<ReturnType<Handler>, number> {
+	const count = new Map<ReturnType<Handler>, number>()
 
 	for (const [key, value] of toEntries(collection)) {
 		// cast to never because for some reason TS says params are never
-		const result = handler(key as never, value as never)
+		const result = handler(value as never, key as never)
+		const currentCount = count.get(result) || 0
 
-		if (count[result] === undefined) count[result] = 0
-
-		count[result]++
+		count.set(result, currentCount + 1)
 	}
 
 	return count
