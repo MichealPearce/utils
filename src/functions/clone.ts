4ddb1f38ef698e860f7fb7ea3af4773034e88433
Micheal Pearce <michealpearce@outlook.com>
2023-03-14 19:44:43 -0400

    add more checks to cloneDeep

--- src/functions/clone.ts
+++ src/functions/clone.ts
@@ -44,6 +44,8 @@ export function clone<A>(target: A): A {
  */
 export function cloneDeep<A>(target: A): A {
 	if (isArray(target)) return target.map(cloneDeep) as A
+	else if (isSet(target)) return new Set(target) as A
+	else if (isMap(target)) return new Map(target) as A
 	else if (isObject(target))
 		return fromEntries(
 			toEntries(target).map(([key, value]) => [key, cloneDeep(value)]),
