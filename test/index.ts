37e16276ef53b06462402af9c6954ab217255935
Micheal Pearce <michealpearce@outlook.com>
2023-03-18 00:24:56 -0400

    refactored to use vite for build process

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,14 @@
-import { testGroup } from './group'
-import { testIs } from './is'
-import { testDifference } from './difference'
+// import { testGroup } from './group'
+// import { testIs } from './is'
+// import { testDifference } from './difference'
+// import { ConstructStore } from '@michealpearce/utils'
 
-testIs()
-testGroup()
-testDifference()
+// testIs()
+// testGroup()
+// testDifference()
+
+// const store = new ConstructStore({
+// 	name: 'micheal',
+// 	age: 24,
+// 	likes: ['cats', 'dogs', 'birds'],
+// })
