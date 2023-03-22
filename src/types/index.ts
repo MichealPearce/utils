496a209622ec031da48c4033e82c3b8cf074bc45
Micheal Pearce <michealpearce@outlook.com>
2023-03-22 12:49:06 -0400

    implement Nullable, Optional, and UnionToIntersection util types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,12 @@ export * from './ClassType'
 export * from './FunctionType'
 export * from './ObjectMethods'
 export * from './ObjectEntries'
+
+export type Nullable<Type> = Type | null
+export type Optional<Type> = Type | undefined
+
+export type UnionToIntersection<U> = (
+	U extends any ? (k: U) => void : never
+) extends (k: infer I) => void
+	? I
+	: never
