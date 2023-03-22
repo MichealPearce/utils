5b4d61b7f222b1150df123ff0d88c16b57f7cd61
Micheal Pearce <michealpearce@outlook.com>
2023-03-22 12:46:59 -0400

    mark is/notObject and inObject as deprecated

--- src/functions/inObject.ts
+++ src/functions/inObject.ts
@@ -1,5 +1,10 @@
 import { isObject } from './isObject'
 
+/**
+ * Check if a thing is a record-like object (not null, not array) and has all the given properties.
+ *
+ * @deprecated Use `inRecord` instead.
+ */
 export function inObject<
 	T extends Record<any, any>,
 	Props extends string | number | symbol,
--- src/functions/isObject.ts
+++ src/functions/isObject.ts
@@ -2,12 +2,22 @@ import { Is, Not, not } from './is'
 import { notArray } from './isArray'
 import { notNull } from './isNull'
 
+/**
+ * Strictly checks if a value is a record like object. This means that it is not an array, null, or a primitive.
+ *
+ * @deprecated Use `isRecord` instead
+ */
 export function isObject<T extends object>(
 	thing: unknown,
 ): thing is Is<T, object> {
 	return typeof thing === 'object' && notNull(thing) && notArray(thing)
 }
 
+/**
+ * Strictly checks if a value is a record like object. This means that it is not an array, null, or a primitive.
+ *
+ * @deprecated Use `notRecord` instead
+ */
 export function notObject<T>(thing: T): thing is Not<T, object> {
 	return not(isObject(thing))
 }
